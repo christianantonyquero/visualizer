7813ed436ceaed8563b019d099e8ea7ce7c7f779
Christian Antony Quero <christianantonyquero@gmail.com>
2020-05-21 12:15:08 +0800

    Adding hyperlink on clement mihailescu's github project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Visualizer App based from Clement Mihailescu's Project</h1>
+      <h1>Visualizer App based from  <a href="https://clementmihailescu.github.io/Sorting-Visualizer/">Clement Mihailescu's sorting-visualizer</a> </h1>
       <h2>My React App</h2>
       <img src={profileImage} alt="profile-image" />
     </div>
