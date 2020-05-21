04b427cf939ae64e1e4dcf0e691e4ffd83f9da03
Christian Antony Quero <christianantonyquero@gmail.com>
2020-05-21 21:29:38 +0800

    Adding sorting files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import profileImage from './img/quero.jpeg'
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Visualizer App based from  <a href="https://clementmihailescu.github.io/Sorting-Visualizer/">Clement Mihailescu's sorting-visualizer</a> </h1>
+      <h1>Visualizer App based from<a href="https://clementmihailescu.github.io/Sorting-Visualizer/">Clement Mihailescu's sorting-visualizer</a> </h1>
       <h2>My React App</h2>
       <img src={profileImage} alt="profile-image" />
+      <SortingVisualizer></SortingVisualizer>
     </div>
   );
 }
